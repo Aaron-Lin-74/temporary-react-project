7a9ce4da92e04315eafe5820e6e3f23a4b291c8b
Yaotian Lin <tiankd74@gmail.com>
2021-09-15 16:42:16 +1000

    Project 12

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,45 @@
 import React, { useState, useContext } from 'react'
+
+// useContext will need to use it
+const AppContext = React.createContext()
+
+// Create a separate component, wrap the whole app in the AppProvider
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+  // With the children, we can see the other components
+  return (
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openSidebar,
+        openModal,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+// custom hook, reduced the number of imports
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+export { AppContext, AppProvider }
