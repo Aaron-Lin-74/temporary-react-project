35a687b8e6584cec8d1eeb7effb123573c8e7b75
Yaotian Lin <tiankd74@gmail.com>
2021-09-16 20:20:04 +1000

    Project 13

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,46 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { AppProvider, useGlobalContext } from './context'
+import sublinks from './data'
 
-const Navbar = () => {
-  return <h2>navbar component</h2>
+const Navbar = (e) => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+  const displaySubmenu = (e) => {
+    //console.log(e.target.classList.contains('link-btn'))
+    const page = e.target.textContent
+    // return the size of an element and position relative to the viewport
+    const tempBtn = e.target.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2
+    const bottom = tempBtn.bottom - 3
+
+    openSubmenu(page, { center, bottom })
+  }
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='stript' className='nav-logo' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className='nav-links'>
+          {sublinks.map((link, index) => {
+            const { page } = link
+            return (
+              <li key={index}>
+                <button className='link-btn' onMouseOver={displaySubmenu}>
+                  {page}
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+        <button className='btn signin-btn'>Sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,7 +1,47 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { useGlobalContext } from './context'
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext()
+  const container = useRef(null)
+  const [columnLength, setColumnLength] = useState('col-2')
+  useEffect(() => {
+    setColumnLength('col-2')
+    const submenu = container.current
+    const { center, bottom } = location
+    submenu.style.left = `${center}px`
+    submenu.style.top = `${bottom}px`
+
+    if (links.length === 3) {
+      setColumnLength('col-3')
+    }
+    if (links.length > 3) {
+      setColumnLength('col-4')
+    }
+  }, [location, links])
+  return (
+    <aside
+      class={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columnLength}`}>
+        {links.map((link, index) => {
+          const { label, icon, url } = link
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
+    </aside>
+  )
 }
 
 export default Submenu
